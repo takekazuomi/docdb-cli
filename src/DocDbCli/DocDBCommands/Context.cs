9e955625c7474c8e487dcadb7c98671badc447b9
Takekazu Omi <takekazuomi@gmail.com>
2017-04-11 12:13:34 +0900

    add license header

--- src/DocDbCli/DocDBCommands/Context.cs
+++ src/DocDbCli/DocDBCommands/Context.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿/* Context.cs
+ * 
+ * Copyright 2015-2017 Takekazu Omi
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.IO;
-using System.Reflection;
 using Microsoft.Azure.Documents.Client;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
 using Serilog;
 
 namespace DocDB
@@ -100,28 +114,6 @@ namespace DocDB
             }
         }
     }
- 
-    //public class ShouldSerializeContractResolver : DefaultContractResolver
-    //    {
-    //        public static readonly ShouldSerializeContractResolver Instance = new ShouldSerializeContractResolver();
-    //        public static readonly string[] PropertyNames = { };
-
-    //    protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
-    //    {
-    //        JsonProperty property = base.CreateProperty(member, memberSerialization);
-
-    //        if (property.DeclaringType == typeof(Employee) && property.PropertyName == "Manager")
-    //        {
-    //            property.ShouldSerialize =
-    //                instance =>
-    //                {
-    //                    Employee e = (Employee)instance;
-    //                    return e.Manager != e;
-    //                };
-    //    }
-
-    //    return property;
-    //}
 }
 
 
--- src/DocDbCli/DocDbCli/Properties/AssemblyInfo.cs
+++ src/DocDbCli/DocDbCli/Properties/AssemblyInfo.cs
@@ -1,16 +1,31 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿/* AssemblyInfo.cs
+ * 
+ * Copyright 2015-2017 Takekazu Omi
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("DocDbCli")]
+[assembly: AssemblyTitle("DocDb-Cli")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DocDbCli")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
